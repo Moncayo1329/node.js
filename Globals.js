557cec668d90c44423dc3b3ce34a0e316e858427
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-07-26 15:55:13 +0100

    first commit

---
+++ Globals.js
@@ -0,0 +1,6 @@
+// GLOBALS - NO WINDOWS 
+
+// __dirname - path to current directory 
+// __filename - file name
+// module - function to use modules (CommonJS)
+// process - info about env where the program is being executed 
---
+++ modules.js
@@ -0,0 +1,10 @@
+// Modules 
+
+const names = require('./name')
+const sayHi = require('./util')
+
+
+sayHi('susan')
+sayHi(names.jhon)
+sayHi(names.peter)
+
