b823f061aaf1331464b3c3bd5eb6ab7c20b170c2
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-07-27 17:35:59 +0100

    1:27:36

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-const { readFileSync, writeFileSync } = require('fs');
+// call back enssentially what it means is that we run. es decirm que cuando una funcionalidad esta completa  de ahi run the callback. 
+// normally would do with add event listener on a button remember cualquier event. 
 
-const first = readFileSync('./content/subfolder/first.txt', 'utf8');
-const second = readFileSync('./content/subfolder/second.txt', 'utf8');
+const { readFile, writeFile} = require('fs')
 
-writeFileSync('./content/result-readFileSync.txt', 
-`here is the result : ${first}, ${second}`,
-{flag: 'a'}
+readFile('./content/subfolder/first.txt', 'utf8',(err,result)=>{
+if(err){
+    console.log(err)
+    return;
+}
+const first = result;
+readFile('./content/subfolder/second.txt','utf8', (err,result)=>{
+    if(err){
+        console.log(err)
+        return;
+    }
+    const second = result
+    writeFile('./content/result-sync.txt',
+    `Here is the result : ${first}, ${second}`
+    ,(err,result)=>{
+      if(err) {
+        console.log(err);
+        return;
+      }
+      console.log(result)
+    }
+    )
 
-)
+})
+})
 
-console.log(first, second)
