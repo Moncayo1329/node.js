1d595107cde4336fb6794820bfb47f7ff13b23a3
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-10-16 18:09:28 +0100

    paths

--- app.js
+++ app.js
@@ -36,3 +36,16 @@
 
 
 // 2:41:23
+
+// sincrono 
+
+// puedes unir rutas con path.join 
+const path = require('node:path')
+
+// barra separadora de carpetas segun SO 
+console.log(path.sep)
+
+// unir rutas con path.join
+const filePath = path.join('content' , 'subfolder' , 'test.txt')
+
+console.log(filePath)
